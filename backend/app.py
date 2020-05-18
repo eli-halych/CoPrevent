df66fd76581e2b2af6a37631412b6fda1f554dbb
Illia Halych <eli.halych@gmail.com>
2020-05-18 20:45:24 +0200

    ADD: initialized runnable backend

---
+++ backend/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, redirect
+
+
+def create_app():
+
+    app = Flask(__name__)
+
+    @app.route('/')
+    def present():
+        tmp_string = "A map is here"
+        return tmp_string
+
+    return app
+
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
