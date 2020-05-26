8181e9b64fc96a38c8c8a8e7b3d560781a5be7f6
Illia Halych <eli.halych@gmail.com>
2020-05-26 11:58:30 +0200

    ADD:  POST/GET /survey endpoints + tests
    - POST /survey: show personalized insights
    - GET /survey: get a form. Is it necessary?
    - Endpoint tests

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,76 @@
-from flask import Flask, redirect
+from flask import Flask, redirect, request, abort, json, url_for, jsonify
+from werkzeug.exceptions import HTTPException
 
 
 def create_app():
-
     app = Flask(__name__)
 
-    @app.route('/')
-    def present():
+    @app.route('/', methods=['GET'])
+    def get_map():
+        """
+            Show a map
+            :return: TODO Folium?
+        """
+
         tmp_string = "A map is here"
-        return tmp_string
+        response_body = {
+            'data': tmp_string,
+            'success': True
+        }
+        return jsonify(response_body)
+
+    @app.route('/survey', methods=['GET'])
+    def get_survey():
+        """
+            # TODO remove or keep? Do we need GET form? Can frontend do this?
+            Show a survey form
+            :return: TODO
+        """
+
+        tmp_string = "Survey form is here"
+        response_body = {
+            'data': tmp_string,
+            'success': True
+        }
+        return jsonify(response_body)
+
+    @app.route('/survey', methods=['POST'])
+    def post_survey():
+        """
+            Send filled out survey form data
+            :return: prediction and filtered
+        """
+
+        response_data = {}
+
+        try:
+            requested_data = request.data
+        except Exception as e:
+            abort(400)  # bad request
+
+        try:
+            # TODO call an ML model taking in requested_data
+            response_data['prediction'] = 0.0  # TODO apply when ready
+            response_data['message'] = ''  # TODO apply when ready
+            response_data['success'] = True
+            # TODO add other necessary data for the map
+        except Exception as e:
+            abort(422)  # unprocessable entity
+
+        return jsonify(response_data)
+
+    @app.errorhandler(HTTPException)
+    def handle_exception(e):
+        """Return JSON instead of HTML for HTTP errors."""
+        response = e.get_response()
+
+        response.data = json.dumps({
+            "code": e.code,
+            "name": e.name,
+            "description": e.description,
+        })
+        response.content_type = "application/json"
+        return response
 
     return app
 
---
+++ backend/test.py
@@ -0,0 +1,52 @@
+import unittest
+
+from flask import json
+
+from backend.app import create_app
+
+
+class TestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app()
+        self.client = self.app.test_client
+
+    def tearDown(self):
+        pass
+
+    def test_get_map(self):
+        res = self.client().get('/')
+        data = json.loads(res.data)
+
+        self.assertEqual(data['data'], 'A map is here')
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(data['success'])
+
+    def test_get_survey(self):
+        res = self.client().get('/survey')
+        data = json.loads(res.data)
+
+        self.assertEqual(data['data'], 'Survey form is here')
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(data['success'])
+
+    def test_post_survey(self):
+        # TODO fill out when known
+        request_data = json.dumps({
+            'country_region_code': None,
+            'country_region': None,
+            'sub_region_1': None,
+            'sub_region_2': None,
+            'date': None,
+            'still_on_lockdown': False
+        })
+
+        res = self.client().post('/survey', data=request_data)
+        data = json.loads(res.data)
+
+        self.assertEqual(data['prediction'], 0.0)
+        self.assertEqual(data['message'], '')
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(data['success'])
+
+if __name__ == '__main__':
+    unittest.main()
