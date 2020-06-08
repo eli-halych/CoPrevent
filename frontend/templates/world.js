71ae9e91c3593f1879591ea74fe878ff7fbebf66
Joy Wang <joy.wang312@gmail.com>
2020-06-08 15:47:47 -0700

    Finish draft of anchor and hide elements for step2

---
+++ frontend/templates/world.js
@@ -0,0 +1,9 @@
+function clickToShowStep2() {
+  var x = document.getElementById("step2WholeSurvey");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+  document.getElementById("anchorStep2").click();
+}
