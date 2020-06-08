6393d90381bb592253df2654ffb8fb0042349d92
Joy Wang <joy.wang312@gmail.com>
2020-06-08 16:08:55 -0700

    Set js to hide div by default

--- frontend/templates/world.js
+++ frontend/templates/world.js
@@ -1,5 +1,6 @@
 function clickToShowStep2() {
   var x = document.getElementById("step2WholeSurvey");
+  // $("#reply").hide();
   if (x.style.display === "none") {
     x.style.display = "block";
   } else {
