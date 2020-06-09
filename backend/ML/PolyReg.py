6ff8d61a876b7924b258f617739ca77cde896921
Illia Halych <eli.halych@gmail.com>
2020-06-09 17:10:55 +0200

    ADD: polynomial regression placeholders

---
+++ backend/ML/PolyReg.py
@@ -0,0 +1,14 @@
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+
+
+def get_trend_pred(starting_date, prediction_info, united_samples):
+    """
+        # TODO implement increasing/decreasing trend insight
+    """
+
+    # TODO trend
+
+    trend = None
+
+    return trend
